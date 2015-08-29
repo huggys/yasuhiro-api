6aa22ea2fd522c70c14b108668e5a66a1dbcf2d2
uknmr <y.kinmori@gmail.com>
2015-08-30 03:08:56 +0900

    GET 戯言を追加

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ var server = restify.createServer({
 server.listen(3000, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
+
+function getNonsenses(req, res, next) {
+  // return 戯言
+  return next();
+}
+
+// Routes
+server.get('/nonsenses', getNonsenses);
