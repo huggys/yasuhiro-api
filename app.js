2999841eea4b7f6940c393c837d5ab2888ae7455
uknmr <y.kinmori@gmail.com>
2015-09-01 21:24:29 +0900

    JSONを返却するように修正

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ server.use(restify.queryParser({mapParams: false}));
 server.use(restify.jsonp());
 server.use(restify.gzipResponse());
 
+function getNonsenses(req, res, next) {
+  res.contentType = 'json';
+  res.send(200, {hello: 'world'});
+}
+
 server.listen(3000, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
-
-function getNonsenses(req, res, next) {
-  // return 戯言
-  return next();
-}
 
 // Routes
 server.get('/nonsenses', getNonsenses);
