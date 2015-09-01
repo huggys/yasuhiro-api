e09fc31ce6de2f32fa201ce33ad4f4431fd4a974
uknmr <y.kinmori@gmail.com>
2015-09-01 18:50:52 +0900

    Add gzip-response plugin

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var server = restify.createServer({
 // Bundle Plugins
 server.use(restify.queryParser({mapParams: false}));
 server.use(restify.jsonp());
+server.use(restify.gzipResponse());
 
 server.listen(3000, function() {
   console.log('%s listening at %s', server.name, server.url);
