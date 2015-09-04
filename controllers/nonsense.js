244ae235c62d9e785c4760d9e125a2e1ba71070b
uknmr <y.kinmori@gmail.com>
2015-09-05 00:48:27 +0900

    Implement find nonsenses

--- controllers/nonsense.js
+++ controllers/nonsense.js
@@ -1,4 +1,8 @@
+var mongoose = require('mongoose'),
+    Nonsense = mongoose.model('Nonsense');
+
 exports.getNonsenses = function(req, res, next) {
-  res.contentType = 'json';
-  res.send(200, {hello: 'world'});
+  Nonsense.find({confidential: false}, function(err, nonsenses) {
+    res.send(200, nonsenses);
+  });
 }
