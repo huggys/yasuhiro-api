1cf4e951e4dc2f113b9cb50951a1670cc30f4dc1
uknmr <y.kinmori@gmail.com>
2015-09-07 13:17:34 +0900

    Add test 'GET /nonsenses'

--- test/nonsense_test.js
+++ test/nonsense_test.js
@@ -1,3 +1,20 @@
-describe('戯言', () => {
-  it('あとで書く');
+import restify from 'restify';
+
+let client = restify.createJsonClient({
+  url: 'http://localhost:5000',
+  version: '*'
+});
+
+describe('戯言 API', () => {
+  it('GET /nonsenses', (done) => {
+    client.get('/nonsenses', (err, req, res, obj) => {
+      if (err) {
+        throw new Error(err);
+      }
+
+      assert(res.statusCode === 200);
+
+      done();
+    });
+  });
 });
