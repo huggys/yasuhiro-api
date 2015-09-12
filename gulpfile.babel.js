94550b8b446bd456bf8b5f09dcb60d8c142bb867
uknmr <y.kinmori@gmail.com>
2015-09-12 11:47:44 +0900

    lint対象モジュールを明示。
    .gitignoreが効かないため

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,14 @@
 import gulp from 'gulp';
 import eslint from 'gulp-eslint';
 
+const GLOB_SRC_FILES = [
+  './controllers/**/*.js',
+  './core/**/*.js',
+  './models/**/*.js',
+];
+
 gulp.task('lint', () => {
-  return gulp.src('**/*.js')
+  return gulp.src(GLOB_SRC_FILES)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
