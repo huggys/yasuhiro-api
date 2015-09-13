57779c0f8d56ba1aa2fcf1aea70cd6c8882cbcba
uknmr <y.kinmori@gmail.com>
2015-09-13 09:31:06 +0900

    watchしてnpm restartを追加

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,24 @@
 import gulp from 'gulp';
 import eslint from 'gulp-eslint';
+const spawn = require('child_process').spawn;
+let node;
 
 const GLOB_SRC_FILES = [
   './controllers/**/*.js',
   './core/**/*.js',
   './models/**/*.js',
 ];
+
+gulp.task('server', () => {
+  if (node) node.kill();
+
+  node = spawn('node', ['app.js'], {stdio: 'inherit'});
+  node.on('close', (code) => {
+    if (code === 8) {
+      gulp.log('Error detected, waiting for changes...');
+    }
+  });
+});
 
 gulp.task('lint', () => {
   return gulp.src(GLOB_SRC_FILES)
@@ -14,8 +27,10 @@ gulp.task('lint', () => {
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', () => {
-  gulp.watch(GLOB_SRC_FILES, () => {
-    gulp.run('lint');
-  });
+gulp.task('default', ['server'], () => {
+  gulp.watch(GLOB_SRC_FILES, ['lint', 'server']);
+});
+
+process.on('exit', () => {
+  if (node) node.kill();
 });
